6399d4b52f78b846e8316b0cb44ec3d2e038a44e
dd9089 <103137009+dd9089RIT@users.noreply.github.com>
2025-02-24 14:00:58 -0500

    added the IFFT function file. worked on the axis

--- Proj1.m
+++ Proj1.m
@@ -1,10 +1,18 @@
 Trep = 1e-6;
 t = [0:Trep:0.01];
 ftone = 1000;
-xt = cos(2*pi*ftone*t);
-plot(t, xt);grid
-Xjw = fft(xt);
+x = cos(2*pi*ftone*t);
+figure;
+plot(t, x);grid
+
+Xjw = fft(x);
 Xjw = fftshift(Xjw);
 faxis = linspace(-1/Trep/2,1/Trep/2,length(Xjw));
+figure;
 plot(faxis, abs(Xjw));grid
+axis([-2000 2000 -inf inf])
 
+xt = Inv_Fourier(Xjw);
+figure;
+plot(t, xt); grid
+axis([-inf inf -1 1])
